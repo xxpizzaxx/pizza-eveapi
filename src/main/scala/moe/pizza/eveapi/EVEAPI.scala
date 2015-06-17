779029979a212e53f0d84c5cccac70e03aba96e5
Andi Miller <andi@andimiller.net>
2015-06-17 01:53:17 +0100

    added some corp endpoint bindings

--- src/main/scala/moe/pizza/eveapi/EVEAPI.scala
+++ src/main/scala/moe/pizza/eveapi/EVEAPI.scala
@@ -7,7 +7,6 @@ import scala.concurrent.duration._
 class EVEAPI(baseurl: String = "https://api.eveonline.com/")(implicit val key: Option[ApiKey] = None, implicit val ec: ExecutionContext) {
   val account = new endpoints.Account(baseurl, key)
   val char = new endpoints.Character(baseurl, key)
+  val corp = new endpoints.Corp(baseurl, key)
   val eve = new endpoints.Eve(baseurl)
-
-
 }
---
+++ src/main/scala/moe/pizza/eveapi/endpoints/Corp.scala
@@ -0,0 +1,12 @@
+package moe.pizza.eveapi.endpoints
+
+import moe.pizza.eveapi.{ApiRequest, ApiKey}
+import moe.pizza.eveapi.generated.corp
+
+import scala.concurrent.ExecutionContext
+
+class Corp(baseurl: String, apikey: Option[ApiKey]) {
+  def AccountBalance() = new ApiRequest[corp.AccountBalance.Eveapi](baseurl, "corp/AccountBalance.xml.aspx", apikey).apply().map{_.map{_.result.rowset.row}}
+  def ContactList()  = new ApiRequest[corp.ContactList.Eveapi](baseurl, "corp/ContactList.xml.aspx", apikey).apply().map{_.map{_.result.rowset}}
+  def Standings() = new ApiRequest[corp.Standings.Eveapi](baseurl, "corp/Standings.xml.aspx", apikey).apply().map{_.map{_.result.corporationNPCStandings.rowset}}
+}
