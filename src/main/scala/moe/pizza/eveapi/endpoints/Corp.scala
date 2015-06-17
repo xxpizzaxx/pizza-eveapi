63eba932a279d72163e1e5d56a0f38608d3ad05e
Andi Miller <andi@andimiller.net>
2015-06-17 00:58:23 +0000

    added implicit execution context for corp

--- src/main/scala/moe/pizza/eveapi/endpoints/Corp.scala
+++ src/main/scala/moe/pizza/eveapi/endpoints/Corp.scala
@@ -5,7 +5,7 @@ import moe.pizza.eveapi.generated.corp
 
 import scala.concurrent.ExecutionContext
 
-class Corp(baseurl: String, apikey: Option[ApiKey]) {
+class Corp(baseurl: String, apikey: Option[ApiKey])(implicit ex: ExecutionContext) {
   def AccountBalance() = new ApiRequest[corp.AccountBalance.Eveapi](baseurl, "corp/AccountBalance.xml.aspx", apikey).apply().map{_.map{_.result.rowset.row}}
   def ContactList()  = new ApiRequest[corp.ContactList.Eveapi](baseurl, "corp/ContactList.xml.aspx", apikey).apply().map{_.map{_.result.rowset}}
   def Standings() = new ApiRequest[corp.Standings.Eveapi](baseurl, "corp/Standings.xml.aspx", apikey).apply().map{_.map{_.result.corporationNPCStandings.rowset}}
