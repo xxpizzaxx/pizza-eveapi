cc23d8b5fe786ccffbe5cbd1345be3e516ec37c0
Andi Miller <andi.miller@datasift.com>
2015-10-20 14:30:33 +0100

    make the output from schema/class generation more readable

--- build.sh
+++ build.sh
@@ -8,17 +8,15 @@ mkdir -p src/main/resources/xsd/corp
 echo "Generating XSD schemas"
 for xml in `find src/main/resources/raw/ -name '*.xml'`;
 do
-    echo "\t"$xml
     input=$xml;
     output=`echo $xml | sed 's/raw/xsd/g' | sed 's/\.xml/\.xsd/g'`;
     java -jar util/trang.jar $input $output;
-    echo $output;
+    echo "\t"$xml" -> "$output;
 done
 echo "Generating namespaces for XSD schemas"
 for xsd in `find src/main/resources/xsd/ -name '*.xsd'`;
 do
     p=`echo $xsd | sed 's/.*xsd\//moe.pizza.eveapi.generated./' | sed 's:/:.:' | sed 's/\.xsd$//g'`;
-    echo $p;
-    echo $xsd;
-    java -jar util/scalaxb.jar $xsd -p $p --package-dir --protocol-package $p -d src/main/scala/;
+    echo "\t"$p" -> "$xsd;
+    java -jar util/scalaxb.jar $xsd -p $p --package-dir --protocol-package $p -d src/main/scala/ | sed 's/^/\t\t/g';
 done
