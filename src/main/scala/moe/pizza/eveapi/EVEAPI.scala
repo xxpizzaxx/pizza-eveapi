39ec350ea7056ed36ce0112dcf60318480bc72ac
Andi Miller <andi@andimiller.net>
2016-05-03 14:08:25 +0100

    move implicit to normal argument for ease of use

--- src/main/scala/moe/pizza/eveapi/EVEAPI.scala
+++ src/main/scala/moe/pizza/eveapi/EVEAPI.scala
@@ -4,7 +4,7 @@ import scala.concurrent.{Await, Future, ExecutionContext}
 import scala.concurrent.duration._
 
 
-class EVEAPI(baseurl: String = "https://api.eveonline.com/")(implicit val key: Option[ApiKey] = None, implicit val ec: ExecutionContext) {
+class EVEAPI(baseurl: String = "https://api.eveonline.com/", key: Option[XmlApiKey] = None)(implicit val ec: ExecutionContext) {
   val account = new endpoints.Account(baseurl, key)
   val char = new endpoints.Character(baseurl, key)
   val corp = new endpoints.Corp(baseurl, key)
